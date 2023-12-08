1da85a20e3a5eaf6844ff2ed5eaa4ec45b224a9f
Emanuel Casulo <emanuel.casulo@alumnos.frgp.utn.edu.ar>
2023-12-08 18:26:26 -0300

    primera subida

---
+++ Sol_ProcesoCRUD/ProcesoCRUD/Datos/Conexion.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcesoCRUD.Datos
+{
+    public class Conexion
+    {
+        private string Base;
+        private string Servidor;
+        private string Usuario;
+        private string Clave;
+        private static Conexion con = null;
+
+        private Conexion()
+        {
+            this.Base = "BD_CRUD_CURSO";
+            this.Servidor = "Manulo-PC\\SQLLABO";
+            this.Usuario = "ecasulo";
+            this.Clave = "12345678";
+        }
+        /*
+         * Creamos la conexion de la base de datos
+         */
+        public SqlConnection CrearConexion()
+        {
+            SqlConnection cadena = new SqlConnection();
+            try
+            {
+                cadena.ConnectionString = "server=" + this.Servidor +
+                                          "; database=" + this.Base +
+                                          "; user=" + this.Usuario +
+                                          "; password=" + this.Clave +
+                                          ";";
+            }
+            catch (Exception ex)
+            {
+                cadena = null;
+                throw ex;
+            }
+            return cadena;
+        }
+
+        /* 
+         * Para evitar que se cree una nueva conexion a la base de datos
+         * Se crea un metodo estatico que devuelva la misma conexion
+         * Si la conexion es nula, se crea una nueva 
+        */
+        public static Conexion getInstancia()
+        {
+            if (con == null)
+            {
+                con = new Conexion();
+            }
+            return con;
+        }
+    }
+}
---
+++ Sol_ProcesoCRUD/ProcesoCRUD/Entidades/E_Productos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProcesoCRUD.Entidades
+{
+    public class E_Productos
+    {
+        public int cod_prod { get; set; }
+        public string descrip_prod { get; set; }
+        public string marca_prod { get; set; }
+        public int codigo_me { get; set; }
+        public int cod_cat { get; set; }
+        public decimal stock_actual { get; set; }
+    }
+}
